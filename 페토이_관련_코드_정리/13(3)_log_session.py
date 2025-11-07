63f9e5f4ad220c08b91506b61651d21193c9c02c
doyeonryu <doyeonryu.ryu@gmail.com>
2025-11-07 12:12:10 +0900

    modified file name

--- 페토이_관련_코드_정리/13(3)_log_session.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("./logs/도연_session.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-for msg in data:
-    role = msg["type"]
-    content = msg["data"]["content"]
-    print(f"{role}: {content}")
